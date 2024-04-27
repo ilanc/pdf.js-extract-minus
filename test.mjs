5fd7c84f0d746e63cdb3bb3a2b823c2ce4c47bb8
ilan <ilan@spikedata.co.za>
2024-04-27 12:52:50 +0200

    test demo

--- test.mjs
+++ test.mjs
@@ -2,9 +2,11 @@ import { readFileSync, writeFileSync } from "fs";
 import pdfJs from "pdfjs-dist/build/pdf.js"; // using pdfjs-dist v2.6.347 works - preserves unicode minuses
 import { replaceUnicodeChars } from "./unicode.mjs";
 
+const __dirname = import.meta.url.substr(7).replace(/[\\/][^\\/]*$/, "");
+
 const defaultOptions = {
   getDocument: {
-    verbosity: -1,
+    // verbosity: -1,
   },
   getTextContent: {
     // normalizeWhitespace: false,
@@ -18,18 +20,25 @@ async function extractPage1(
   getTextContentOptions = defaultOptions.getTextContent
 ) {
   const options = { ...getDocumentOptions, data: new Uint8Array(buffer) };
-  const doc = await pdfJs.getDocument(options);
-  const text = await loadPage(1, getTextContentOptions);
+  const doc = await pdfJs.getDocument(options).promise;
+  const text = await loadPage(1, doc, getTextContentOptions);
   return text;
 }
 
-async function loadPage(pageNum, options = defaultOptions.getTextContent) {
+async function loadPage(pageNum, doc, options = defaultOptions.getTextContent) {
   const page = await doc.getPage(pageNum);
+  const viewport = page.getViewport({ scale: 1.0 });
   const content = await page.getTextContent(options);
-  return content.items.map((item) => ({
-    str: item.str,
-    y: item.y,
-  }));
+  return content.items.map((item) => {
+    const tm = item.transform;
+    const x = tm[4];
+    const y = viewport.height - tm[5];
+    return {
+      str: item.str,
+      x,
+      y,
+    };
+  });
 }
 
 async function run(filename, password, yLine) {
@@ -38,18 +47,17 @@ async function run(filename, password, yLine) {
     : { normalizeWhitespace: false, disableCombineTextItems: false };
   const buffer = readFileSync(filename);
   const raw = await extractPage1(buffer);
-  let line = raw.filter((x) => x.y === yLine);
+  const line = raw.filter((x) => x.y === yLine);
   replaceUnicodeChars(line);
+
+  // check whether minuses are preserved on the line?
   const fixed = line.map((x) => x.str).join();
   console.log(fixed);
-  let p = filename.slice(0, -4) + ".raw.json";
-  writeFileSync(p, JSON.stringify(raw.data, null, 2));
+
+  /// dump to raw.json
+  const p = filename.slice(0, -4) + ".raw.json";
+  writeFileSync(p, JSON.stringify(raw, null, 2));
   console.log("wrote:", p);
 }
 
-// run("/code/prototype/pdf.js-extract-minus/3.pdf", undefined, 397);
-run(
-  "/code/prototype/pdf.js-extract-minus/4.pdf",
-  undefined,
-  248.10181548000003
-);
+run(__dirname + "/0.pdf", undefined, 248.10181548000003);
